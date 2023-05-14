5e5a04c994f4815c7ee5ff1072ba6f688c7c1061
yuni-0928 <yuniyuni0928@gmail.com>
2023-05-14 20:52:56 +0900

    dotfilesLinkを更新する

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bash_logout ~/.bash_logout
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/asdf/.tool-versions ~/.tool-versions

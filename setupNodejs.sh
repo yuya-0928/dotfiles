029ada39ececab574224cbf6fe8fdfa4f593a236
yuni-0928 <yuniyuni0928@gmail.com>
2023-07-08 00:35:23 +0900

    asdfを使ってインストールするnodejsのバージョンを増やす

--- setupNodejs.sh
+++ setupNodejs.sh
@@ -2,4 +2,7 @@
 # setupNodejs.shは、setupAsdf.shを実行してから実行する。
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs 16.3.0
+asdf install nodejs 18.12.1
+asdf install nodejs 18.14.2
+asdf install nodejs 18.16.1
 asdf global nodejs 16.3.0
